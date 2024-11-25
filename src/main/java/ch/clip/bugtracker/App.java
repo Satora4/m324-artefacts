a5f635bab97da7980c22dd8ad2991bbd4ce2d223
Joel Vontobel <joel.vontobel@ergon.ch>
2024-11-25 16:07:10 +0100

    innitial commit

---
+++ src/main/java/ch/clip/bugtracker/App.java
@@ -0,0 +1,14 @@
+package ch.clip.bugtracker;
+
+/**
+ * @author jvontobe
+ * @version 25.11.2024
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
